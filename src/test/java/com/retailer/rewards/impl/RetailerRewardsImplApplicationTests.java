a5ae6f184a59f856102bc5a72aa89973342e5788
Venu Babu Karri <venu.k@tekyantra.com>
2022-04-27 19:08:01 +0530

    Initial commit

---
+++ src/test/java/com/retailer/rewards/impl/RetailerRewardsImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailer.rewards.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailerRewardsImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
